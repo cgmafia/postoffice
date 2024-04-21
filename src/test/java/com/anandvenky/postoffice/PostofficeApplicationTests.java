fba75ccc21d1e274fab66a10a1534a4494e8acbf
cgmafia <anandvenkatarama@gmail.com>
2024-04-21 12:18:13 +0200

    initial commit

---
+++ src/test/java/com/anandvenky/postoffice/PostofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anandvenky.postoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
